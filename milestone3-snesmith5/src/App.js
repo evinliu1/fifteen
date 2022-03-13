eeb09cba3a82ec3ba60655d377148db0e5520d05
Sade Nesmith <snesmith5@student.gsu.edu>
2022-03-12 22:10:40 -0500

    added map function to app.js

--- milestone3-snesmith5/src/App.js
+++ milestone3-snesmith5/src/App.js
@@ -15,27 +15,29 @@ function comment() {
   // declare new state variable
   // calling it input because it holds userInput (comments/ratings)
   // setInput allows us to update the input
-  const [input, setInput] = useState(input);
+  const [ratings, setRatings] = useState([]);
 
   // userEffect tells react to do 'something' after every render
   useEffect(() => {
     // displays after every render
     document.title = `Here is your new input ${input}`
-  });
-  // comment_list = Map(comment, input);
-
+  })
+};
+// comment_list = Map(comment, input);
+Map(ratings => {
   return (
-
     // when user clicks call setinput with a new value
     // react re-renders comment component pasing new input to it
     <div>
-      <p>Here is your new input {input}</p>
-      <button onClick={() => setInput(input)}>
+      <p>Here is your new input {ratings}</p>
+      <button onClick={() => setInput(ratings)}>
         Update
       </button>
+
     </div>
   )
-};
+}
 
 
+);
 export default App;
